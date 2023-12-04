00ea716b49ef90b483ba520ae3621b73f0390f8b
joshisworking <joshongit@northmail.info>
2023-12-04 10:52:04 -0500

    add Copy-DirectReports

---
+++ Active-Directory/Copy-DirectReports.ps1
@@ -0,0 +1,89 @@
+<#
+.SYNOPSIS
+    Copies direct reports from one Active Directory user to another.
+
+.DESCRIPTION
+    This PowerShell function copies direct reports from a source Active Directory user to a destination user.
+
+.PARAMETER Source
+    Specifies the source Active Directory user account Identity from which group memberships will be copied.
+    Valid inputs include 1) A distinguished name, 2) A GUID (objectGUID), 3) A security identifier (objectSid), or 4) A SAM account name (sAMAccountName) 
+    This parameter is mandatory.
+
+.PARAMETER Destination
+    Specifies the destination Active Directory user account Identity from which group memberships will be copied.
+    Valid inputs include 1) A distinguished name, 2) A GUID (objectGUID), 3) A security identifier (objectSid), or 4) A SAM account name (sAMAccountName) 
+    This parameter is mandatory.
+
+.EXAMPLE
+    Copy-Memberships -Source <ADUser Identity> -Destination <ADUser Identity>
+
+.NOTES
+    Author: Josh Dodd
+    Last update: 4 December 2023
+
+    To use this function, make sure you are running it in an environment with Active Directory cmdlets available.
+#>
+
+function Copy-DirectReports {
+    Param(
+        [Parameter (Mandatory)]
+        [string]$Source, 
+
+        [Parameter (Mandatory)]
+        [string]$Destination
+    )
+    
+    # Obtain the source user, end operation and show error if failed
+    try {
+        [Microsoft.ActiveDirectory.Management.ADAccount]$sourceManager = Get-ADUser $Source -Properties directReports -ErrorAction Stop
+    }
+    catch {
+        Write-Error "Error: Unable to obtain Source AD User. Please check that name is correct."
+        return
+    }
+
+    # Obtain the destination user, end operation and show error if failed
+    try {
+        [Microsoft.ActiveDirectory.Management.ADAccount]$destinationManager = Get-ADUser $Destination -ErrorAction Stop
+    }
+    catch {
+        Write-Error "Error: Unable to obtain Destination AD User. Please check that name is correct."
+        return
+    }
+
+    # If no direct reports, write message and end operation
+    $reports = $sourceManager | Select-Object -ExpandProperty directReports
+    if ($reports.Count -eq 0) {
+        Write-Output "No direct reports found for source user. Terminating."
+        return
+    }
+
+    # Print current activity 
+    Write-Output "Reassigning direct reports from $($sourceManager.Name) to $($destinationManager.Name)"
+    Write-Output "----------------------------------------------------------"
+    
+    # Set destination user as the manager for all direct reports. 
+    # Success/Fail count will be incremented as manager is set for reports
+    $successCount = 0
+    $failedCount = 0
+    foreach ($person in $reports) {
+        try {
+            Set-ADUser $person -Manager $destinationManager -ErrorAction Stop
+            Write-Output "Manager successfully updated for: $person"
+            $successCount += 1
+        }
+        catch {
+            Write-Warning "Could not update manager for $person"
+            $failedCount += 1
+        }
+    }
+
+    Write-Output "`n------------------------------------"
+    Write-Output "Operation complete."
+    Write-Output "------------------------------------"
+    Write-Output "Direct reports total count: $($reports.Count)"
+    Write-Output "Direct reports moved: $successCount."
+    Write-Output "Direct reports failed: $failedCount"
+}
+
--- README.md
+++ README.md
@@ -13,23 +13,23 @@ Functions can be loaded to a PowerShell session by dot-sourcing the file and cal
     PS> . Path\To\FunctionName.ps1
     PS> FunctionName -Arguments $true
 
-## Scripts
+## Contents
 
 ### Active Directory
 - [Copy-ADMemberships](./Active-Directory/Copy-ADMemberships.ps1)
-Copies memberships from one Active Directory user to another, with user interaction.
+Function. Copies memberships from one Active Directory user to another, with user interaction.
+- [Copy-DirectReports](./Active-Directory/Copy-DirectReports.ps1)
+Function. Copies direct reports from one Active Directory user to another, replacing previous manager.
 
 ### Azure
 - [Get-AzureAppPermissionReport](./Azure/Get-AzureAppPermissionReport.ps1)
-Audits and reports on permissions associated with Azure Active Directory applications and their corresponding service principals.
+Script. Audits and reports on permissions associated with Azure Active Directory applications and their corresponding service principals.
 - [Get-EXOMailboxDiscrepancies](./Azure/Get-EXOMailboxDiscrepancies.ps1)
-Lists Active Directory on-premises accounts with missing Exchange Online mailboxes and orphaned EXO mailboxes.
-
-## Functions
+Script. Lists Active Directory on-premises accounts with missing Exchange Online mailboxes and orphaned EXO mailboxes.
 
 ### General Utilities
 - [Save-AclPermissions](./General%20Utilities/Save-ACLPermissions.ps1)
-Collects and saves security permissions for a specified folder, subfolders and (optionally) all sub-items.
+Function. Collects and saves security permissions for a specified folder, subfolders and (optionally) all sub-items.
 
 ## License
 MIT License
